a096fc674a040e394f012e53ce7c1034065db2c6
Des Chandhok <deschandhok24@gmail.com>
2025-10-31 15:11:32 -0400

    Remove dist ts changes- added this change to gitignore as well

--- backend/dist/db/events.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Get all events, optionally filtered by start and end date
- * @param page page number (1-based)
- * @param limit number of items per page
- * @param startDate optional
- */
-export declare const getAllEvents: (page: number, limit: number, startDate?: Date | null) => Promise<any[]>;
--- backend/dist/db/index.js
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient(); // uses pool from DATABASE_URL
-export default prisma;
-//# sourceMappingURL=index.js.map
